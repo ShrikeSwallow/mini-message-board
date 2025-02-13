8acab71c2b58ccc47bc3254eaecb13f9e45aa630
ShrikeSwallow <maciej.jaskolka@gmail.com>
2025-02-13 06:13:07 +0000

    Add new message form and implement post handling

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,19 @@ const messages = [
   },
 ];
 
+router.get("/new", (req, res, next) => {
+  res.render("form", { title: "Add new message" });
+});
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+router.post("/new", (req, res, next) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
